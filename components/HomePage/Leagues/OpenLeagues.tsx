f9b2d54c7a46f9864a38a97d7daa1edba048e7c3
unknown <hunchographics@gmail.com>
2022-12-13 17:13:11 +0300

    tables uniform

--- components/HomePage/Leagues/OpenLeagues.tsx
+++ components/HomePage/Leagues/OpenLeagues.tsx
@@ -1,6 +1,8 @@
 import k from './Leagues.module.css';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const OpenLeagues = ({ league, prize }: { league: any, prize: any }) => { 
@@ -8,7 +10,7 @@ const OpenLeagues = ({ league, prize }: { league: any, prize: any }) => {
   return (
 
     <div className={`${k.resultsRow} h-10`}>
-        <span className="text-sm">${Intl.NumberFormat('en-US').format(prize)}</span>  <span className="text-sm">{league.region}</span> <span className="text-sm">${Intl.NumberFormat('en-US').format(league.buyInFee)}</span> <span className="text-sm">{Math.ceil(league.duration / 7) + ' weeks'}</span> <a href={`/league-summary/${league.name}/`} className="outline outline-[#ff921b] outline-1 text-sm bg-gray-dark px-3 rounded-xl capitalize" >View</a></div>)
+      <span className="text-sm flex items-center space-x-2 "><span className=" bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">{Intl.NumberFormat('en-US').format(prize)}</span><FontAwesomeIcon icon={ faCoins} className="text-secondary" /></span>  <span className="text-sm">{league.region}</span> <span className="text-sm flex space-x-2 items-center"><span className=" bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">{Intl.NumberFormat('en-US').format(league.buyInFee)}</span><FontAwesomeIcon icon={ faCoins} className="text-secondary" /></span> <span className="text-sm">{Math.ceil(league.duration / 7) + ' weeks'}</span> <a href={`/league-summary/${league.name}/`} className="outline outline-[#ff921b] outline-1 text-sm bg-gray-dark px-3 rounded-xl capitalize" >View</a></div>)
 
 }
 
